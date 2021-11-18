255105b973eefdf0e509aa44ae397f33ae6b3195
flavio2018 <flavio.petruzzellis@gmail.com>
2021-11-18 17:02:01 +0100

    Change compat.v1.flags to app.flags; bypass __parsed check

--- neuralgpu/model.py
+++ neuralgpu/model.py
@@ -25,7 +25,7 @@ from . import mytf
 from . import data_utils
 from .records import NeuralGPUResult
 
-FLAGS = tf.compat.v1.flags.FLAGS
+FLAGS = tf.app.flags.FLAGS
 
 
 def tf_cut_function(val, vlo, vhi, glo, ghi):
